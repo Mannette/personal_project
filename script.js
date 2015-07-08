465583a837f1fc226040266f6e1b46023abad030
ethan <ethanmannette@gmail.com>
2015-07-08 11:07:30 -0600

    Added additional jquery code. Additional html and css updates.

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-$(document)ready(function({
+$(document)ready(function(){
 
   var $button = $(".button")
   $button.click
+});
 
-  $(".Ottawa")mouseenter(function({
-    
-  }))
-})
+$(document)ready(function(){
+  var $Ottawa = $(".Ottawa")
+  $Ottawa.hover(function() {
+    $Ottawa.fadeTo("#ff1c00")
+  });
+  var $Canada = $(".Canada")
+  $Canada.mouseenter(function(){
+    $Canada.fadeTo("#ffffff").
+  });
+});
